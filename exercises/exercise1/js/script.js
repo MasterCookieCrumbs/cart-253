f76b82e7743082996bef7a09fae0708a80d68637
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-09-24 16:46:22 -0400

    Testing translate

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -2,19 +2,34 @@
 Template p5 project
 Pippin Barr
 
-Here is a description of this template p5 project.
+This is exercise 1
 **************************************************/
+// let flutter = 2;
+
+let fSize = 10;
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-
+  createCanvas(800, 800);
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+let fpositionX = mouseX;
+let fpositionY = mouseY;
+
+  background(150);
+  noStroke();
+  fill(205, 15, 15 , 100);
+
+
+
+translate(width / 3, height / 9);
+translate(p5.Vector.fromAngle(millis() / 400, 100));
+  ellipse(fpositionX, fpositionY, fSize);
 
 }
