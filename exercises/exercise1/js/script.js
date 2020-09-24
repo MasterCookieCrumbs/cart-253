b4546114858f1baea9d7acd242d93158672d7d67
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-09-24 19:53:01 -0400

    Added Object

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -1,35 +1,44 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Based from the Template p5 project
+Emile Simard
 
 This is exercise 1
 **************************************************/
+
+let fly = {
+//  positionX: 0,
+//  positionY: 0,
+  size: 15,
+  r: 255,
+  g: 155,
+  b: 50
+};
+
+let v1 = createVector(mouseX, mouseY);
 // let flutter = 2;
 
-let fSize = 10;
-
 // setup()
-//
-// Description of setup() goes here.
+
 function setup() {
   createCanvas(800, 800);
 }
 
 // draw()
-//
-// Description of draw() goes here.
+
 function draw() {
-let fpositionX = mouseX;
-let fpositionY = mouseY;
+let positionX = mouseX;
+let positionY = mouseY;
+// flicker is what adds a little variation to each variable
+let flicker = random(-10, 10);
 
-  background(150);
+  background(5, 5, 5, 10);
   noStroke();
-  fill(205, 15, 15 , 100);
+  fill(fly.r, fly.g, fly.b, 300 + (flicker) * 10);
 
-
-
-translate(width / 3, height / 9);
-translate(p5.Vector.fromAngle(millis() / 400, 100));
-  ellipse(fpositionX, fpositionY, fSize);
+//translate(p5.Vector.fromAngle(millis() * 0.003 , 80));
+  ellipse(positionX, positionY, fly.size + flicker);
+  translate(40 + random(-3,3), 40 + random(-3,3));
+translate(v1.x, v1.y);
+  ellipse(positionX, positionY, fly.size + flicker);
 
 }
