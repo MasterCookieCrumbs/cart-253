73339321b6f8629164767590db6fbc4423f47ee7
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-09-24 20:07:05 -0400

    Messing around with Vectors

--- exercises/exercise1/js/script.js
+++ exercises/exercise1/js/script.js
@@ -6,16 +6,13 @@ This is exercise 1
 **************************************************/
 
 let fly = {
-//  positionX: 0,
-//  positionY: 0,
   size: 15,
   r: 255,
   g: 155,
   b: 50
 };
 
-let v1 = createVector(mouseX, mouseY);
-// let flutter = 2;
+//let v1 = createVector(mouseX, mouseY);
 
 // setup()
 
@@ -35,10 +32,16 @@ let flicker = random(-10, 10);
   noStroke();
   fill(fly.r, fly.g, fly.b, 300 + (flicker) * 10);
 
-//translate(p5.Vector.fromAngle(millis() * 0.003 , 80));
-  ellipse(positionX, positionY, fly.size + flicker);
-  translate(40 + random(-3,3), 40 + random(-3,3));
-translate(v1.x, v1.y);
+//the following line is a modified version of an example on the p5.js library
+translate(p5.Vector.fromAngle(millis() * 0.003 , 90));
+
   ellipse(positionX, positionY, fly.size + flicker);
 
+  fill(fly.r, fly.g, fly.b, 300 + (flicker) * 10);
+  translate(40 + random(-3,3), 40 + random(-3,3));
+  ellipse(positionX, positionY, fly.size + flicker);
+
+  fill(fly.r, fly.g, fly.b, 300 + (flicker) * 10);
+  translate(-20 + random(-3,3), -60 + random(-3,3));
+  ellipse(positionX, positionY, fly.size + flicker);
 }
