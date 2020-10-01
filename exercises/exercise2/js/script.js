4a05ade62f09c0e2368918bf340b81278139031d
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-10-01 16:43:03 -0400

    E2: Foundation of simulation
    + Obstacle object
    + Obstacle moving across screen
    + Obstacle reseting when it reaches other side to random pos on the opposite side.

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -5,16 +5,44 @@ Pippin Barr
 Here is a description of this template p5 project.
 **************************************************/
 
+let obstacle = {
+  size: 20,
+  x: 800,
+  y: 800,
+  speed: -5,
+  velocityx: 0,
+  velocityy: 0,
+  fill: {
+    r: 255,
+    g: 0,
+    b: 155
+  }
+};
+
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
+  createCanvas(800, 800);
 
+  obstacle.y = random(0,800);
+  obstacle.velocityx = obstacle.speed;
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
+  background(0);
 
+  obstacle.x = obstacle.x + obstacle.velocityx;
+  obstacle.y = obstacle.y + obstacle.velocityy;
+
+  fill(obstacle.fill.r, obstacle.fill.g, obstacle.fill.b);
+  ellipse(obstacle.x, obstacle.y, obstacle.size);
+
+  if (obstacle.x < 0) {
+    obstacle.x = 800;
+    obstacle.y = random(0,800);
+  }
 }
