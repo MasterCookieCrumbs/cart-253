a77690976f22471580ca82c936bc9071bf06f850
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-10-01 23:38:15 -0400

    E2: Added Comments
    + Added comments detailing code
    + Added Else If statement to make the error message go in reverse from time to time

--- exercises/exercise2/js/script.js
+++ exercises/exercise2/js/script.js
@@ -1,10 +1,11 @@
 /**************************************************
-Template p5 project
-Pippin Barr
+Exercise 2
+Emile Simard
 
-Here is a description of this template p5 project.
+Windows XP Simulator
 **************************************************/
 
+// All of our objects and their properties
 let rover = {
   x: 50,
   y: 50,
@@ -17,7 +18,7 @@ let badfile1 = {
   y: 800,
   speed: -10,
   velocityx: 0,
-  velocityy: 0,
+  velocityy: 1,
 };
 
 let badfile2 = {
@@ -26,10 +27,11 @@ let badfile2 = {
   y: 200,
   speed: -10,
   velocityx: 0,
-  velocityy: 0,
+  velocityy: -1,
 };
 
 let errormessage = {
+  reverse: 0,
   size: 20,
   x: 800,
   y: 400,
@@ -38,7 +40,7 @@ let errormessage = {
   velocityy: 0,
 };
 
-
+// Loads all our images
 let dog
 let pngfile
 let successfail
@@ -50,8 +52,7 @@ function preload() {
 
 
 // setup()
-//
-// Description of setup() goes here.
+//  Runs the code in this section once at the beginning
 function setup() {
   createCanvas(800, 800);
 
@@ -64,13 +65,15 @@ function setup() {
 }
 
 // draw()
-//
-// Description of draw() goes here.
+//  Runs the code in this section every frame
 function draw() {
-  background(0);
+background (0);
 
+
+// Draws the dog
   image(dog, rover.x, rover.y);
 
+// Moves dog if arrow keys are pressed down
   if (keyIsDown(RIGHT_ARROW)) {
     rover.x += rover.speed;
   }
@@ -88,45 +91,71 @@ function draw() {
   }
 
 
-  push();
-    errormessage.x = errormessage.x + errormessage.velocityx;
-    errormessage.y = errormessage.y + errormessage.velocityy;
-
-    translate (errormessage.x, errormessage.y);
-      image(successfail, 10, 10, 300, 200);
-
-    if (errormessage.x < -300) {
-      errormessage.x = 800;
-      errormessage.y = random(0,800);
-    }
-  pop();
-
 push();
+//  Moves file to the left as and up/down
   badfile1.x = badfile1.x + badfile1.velocityx;
   badfile1.y = badfile1.y + badfile1.velocityy;
-
-  translate (badfile1.x, badfile1.y);
-  image(pngfile, 10, 10, 80, 80);
+//  Draws the file
+  image(pngfile, badfile1.x, badfile1.y, 80, 80);
 
   if (badfile1.x < -100) {
-    badfile1.x = 800;
-    badfile1.y = random(0,800);
+    badfile1.x = width;
+    badfile1.y = random(0,height);
   }
 pop();
 
 push();
-  badfile2.x = badfile2.x + badfile1.velocityx;
-  badfile2.y = badfile2.y + badfile1.velocityy;
-
-  translate (badfile2.x, badfile2.y);
-  image(pngfile, 10, 10, 80, 80);
+//  Moves file to the left as and up/down
+  badfile2.x = badfile2.x + badfile2.velocityx;
+  badfile2.y = badfile2.y + badfile2.velocityy;
+//  Draws the file
+  image(pngfile, badfile2.x, badfile2.y, 80, 80);
 
   if (badfile2.x < -100) {
-    badfile2.x = 800;
-    badfile2.y = random(0,800);
+    badfile2.x = width;
+    badfile2.y = random(0,height);
   }
 pop();
 
+
+push();
+//  Moves error message to the left
+  errormessage.x = errormessage.x + errormessage.velocityx;
+  errormessage.y = errormessage.y + errormessage.velocityy;
+//  Draws the error message
+    image(successfail, errormessage.x, errormessage.y, 360, 240);
+
+//  Checks to see if error message moves offscreen to the left to place it back on the right
+  if (errormessage.x < -360) {
+    errormessage.x = width;
+    errormessage.y = random(0, height - 200);
+//  Adds the posibility that the error message goes in reverse once it reaches the left
+    if (errormessage.reverse = 1) {
+      errormessage.speed = errormessage.speed * -1;
+    } else {
+      errormessage.reverse = random(0,1);
+    }
+  }
+//  Checks to see if error message has made it's way back to set it back to it's original settings
+    if (errormessage.x > 1360) {
+      errormessage.x = width;
+      errormessage.y = random(0, height - 200);
+      errormessage.speed = errormessage.speed * -1
+    }
+
+pop();
+
+// Checks distance from dog and files to avoid, ends game if too close
+let d1 = int(rover.x, rover.y, badfile1.x, badfile1.y);
+let d2 = int(rover.x, rover.y, badfile2.x, badfile2.y);
+if (d1 < 20){
+//if ((badfile2.y + 20 > rover.y) && (badfile2.y  - 20 < rover.y) && (badfile2.x + 20 > rover.x) && (badfile2.x < rover.x)) {
+  noLoop();
+}
+
+if (d2 < 20){
+  noLoop();
+}
 
 
 }
