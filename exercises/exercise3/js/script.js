1a4559505511afab4f19f97a040305af6cd39da0
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-10-08 17:03:03 -0400

    E3: Added collision boxes
    + Border collision check
    + Circles collision check

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -39,11 +39,24 @@ function setup() {
 function draw() {
   background(0);
 
+
+
   ellipse(circle1.x, circle1.x, circle1.size);
   ellipse(circle2.x, circle2.x, circle2.size);
 
   circle1.x = circle1.x + circle1.x_velocity;
   circle2.x = circle2.x + circle2.x_velocity;
+
   circle1.y = circle1.y + circle1.y_velocity;
   circle2.y = circle2.y + circle2.y_velocity;
+
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height );{
+
+  }
+
+  let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
+  if (d < circle1.size/2 + circle2.size/2){
+
+  }
+
 }
