5805ce0b82128a46c4177b57eca29ee003de3541
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-10-09 00:23:20 -0400

    E3: New functions and states
    + Win state
    + Border bouncing for circles
    + Collision check

--- exercises/exercise3/js/script.js
+++ exercises/exercise3/js/script.js
@@ -1,79 +1,103 @@
 /**************************************************
-Template p5 project
+Exercise 3
 Emile Simard
 
 **************************************************/
 
 let circle1 = {
-  x: 0,
+  x: 50,
   y: 600,
-  size: 100,
+  size: 150,
   x_velocity: 1,
   y_velocity: 1,
-  speed: 3
+  speed: 4
 };
 
 let circle2 = {
-  x: 0,
+  x: 50,
   y: 300,
-  size: 100,
+  size: 150,
   x_velocity: 1,
   y_velocity: 1,
-  speed: 3
+  speed: 4
 };
+
+let state = `title`;
+
 
 function setup() {
   createCanvas(900, 900);
-  state = `title`;
+  circleSetup();
 
-  circle1.x = width/3;
-  circle2.x = 2*width/3;
-  circle1.y = height/3;
-  circle2.y = 2*height/3;
-
-  circle1.x_velocity = random(-circle1.speed,circle1.speed);
-  circle2.x_velocity = random(-circle2.speed,circle2.speed);
-  circle1.y_velocity = random(-circle1.speed,circle1.speed);
-  circle2.y_velocity = random(-circle2.speed,circle2.speed);
 }
 
 function draw() {
   background(0);
-  simulation();
 
   if (state === `title`) {
     title();
-  }
-  else if (state === `simulation`) {
+  } else if (state === `simulation`) {
     simulation();
-  }
-  else if (state === `win`) {
-
-  }
-  else if (state === `loss`) {
+  } else if (state === `win`) {
+    win();
+  } else if (state === `loss`) {
 
   }
 }
 
+
+
 function title() {
-textSize(48);
-fill(200,100,100);
-textAlign(CENTER,CENTER);
-text(`Will they find Love?`, width/2, height/2);
+  // starting text
+  push();
+  textSize(48);
+  fill(255, 150, 150);
+  textAlign(CENTER, CENTER);
+  text(`How long untill they find eachother?`, width / 2, height / 2);
+  pop();
 }
 
 function simulation() {
+  // functions that will run during the simulation
   move();
-  borderCollision();
-  circleCollision();
   circles();
+  borderCollisionCircle1();
+  borderCollisionCircle2();
+  circleCollision()
 }
 
-function keyPressed() {
-  state = `simulation`;
+function circleSetup() {
+  // puts the circles in their starting locations
+  circle1.x = width / 3;
+  circle2.x = 2 * width / 3;
+  circle1.y = height / 3;
+  circle2.y = 2 * height / 3;
+
+  circle1.x_velocity = random(-circle1.speed, circle1.speed);
+  circle2.x_velocity = random(-circle2.speed, circle2.speed);
+  circle1.y_velocity = random(-circle1.speed, circle1.speed);
+  circle2.y_velocity = random(-circle2.speed, circle2.speed);
+}
+
+function win() {
+  // win state with message
+  push();
+  textSize(48);
+  fill(150, 255, 150);
+  textAlign(CENTER, CENTER);
+  text(`They've found Love`, width / 2, height / 2);
+  pop();
+}
+
+function mousePressed() {
+  if (state === `title`) {
+    state = `simulation`;
+    // click to start the simulation
+  }
 }
 
 function circles() {
+  // draws both circles
   ellipse(circle1.x, circle1.x, circle1.size);
   ellipse(circle2.x, circle2.x, circle2.size);
 }
@@ -87,17 +111,26 @@ function move() {
   circle2.y = circle2.y + circle2.y_velocity;
 }
 
-function borderCollision() {
-  // checks if a circle collides with the border of the Canvas
-  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height || circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height );{
-  state = `loss`;
+function borderCollisionCircle1() {
+  // checks if a circle1 collides with the border of the Canvas and changes it's speed
+  if (circle1.x < 0 || circle1.x > width || circle1.y < 0 || circle1.y > height) {
+    circle1.x_velocity = random(-circle1.speed, circle1.speed);
+    circle1.y_velocity = random(-circle1.speed, circle1.speed);
+  }
+}
+
+function borderCollisionCircle2() {
+  // checks if circle2 collides with the border of the Canvas and changes it's speed
+  if (circle2.x < 0 || circle2.x > width || circle2.y < 0 || circle2.y > height) {
+    circle2.x_velocity = random(-circle2.speed, circle2.speed);
+    circle2.y_velocity = random(-circle2.speed, circle2.speed);
   }
 }
 
 function circleCollision() {
   // checks if both circles collide
-  let d = dist(circle1.x,circle1.y,circle2.x,circle2.y);
-  if (d < circle1.size/2 + circle2.size/2){
-  state = `win`;
+  let d = dist(circle1.x, circle1.y, circle2.x, circle2.y);
+  if (d < circle1.size / 2 + circle2.size / 2) {
+    state = `win`;
   }
 }
