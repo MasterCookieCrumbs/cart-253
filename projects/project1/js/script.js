60cd55e3d7db4479afe7d7c581a6c7f6a9d3cdeb
MasterCookieCrumbs <SirMilesjr@gmail.com>
2020-10-15 23:20:11 -0400

    P1: Added Wall Collision
    + Wall collisions
    + Border constraints

--- projects/project1/js/script.js
+++ projects/project1/js/script.js
@@ -5,11 +5,9 @@ Emile Simard
 **************************************************/
 
 let player = {
-  x: 450,
+  x: 30,
   y: 450,
-  size: 10,
-//  x_velocity: 2,
-//  y_velocity: 2,
+  size: 15,
   speed: 8
 };
 
@@ -22,20 +20,76 @@ let entity2 = {
   speed: 2
 };
 
+let gridModifier = 90;
+let wallWidth = 4;
+
+let horizontalWall1 = {
+  x1: 0*(gridModifier),
+  y1: 2*(gridModifier)-(wallWidth),
+  x2: 1*(gridModifier),
+  y2: 2*(gridModifier)+(wallWidth)
+};
+let horizontalWall2 = {
+  x1: 0*(gridModifier),
+  y1: 8*(gridModifier)-(wallWidth),
+  x2: 1*(gridModifier),
+  y2: 8*(gridModifier)+(wallWidth)
+};
+
+
+
 let state = `title`;
 
 
 function setup() {
   createCanvas(900, 900);
-  circleSetup();
 
 }
+
 
 function draw() {
   background(0);
   borderCollisionplayer();
+  wallCollisionplayer();
+
+//print(horizontalWall1.y1);
+
+push();
+  strokeWeight(1);
+  stroke(250, 75, 0);
+  noFill();
+
+// Horizontal Walls
+quad(horizontalWall1.x1, horizontalWall1.y1,  horizontalWall1.x1, horizontalWall1.y2,  horizontalWall1.x2, horizontalWall1.y2,  horizontalWall1.x2, horizontalWall1.y1);
+// Example: quad(horizontalWall(i).x1, horizontalWall(i).y1
 
 
+
+
+
+
+
+// Vertical Walls
+quad(1*(gridModifier)-(wallWidth), 0*(gridModifier),  1*(gridModifier)+(wallWidth), 0*(gridModifier),  1*(gridModifier)+(wallWidth), 1*(gridModifier),  1*(gridModifier)-(wallWidth), 1*(gridModifier));
+quad(2*(gridModifier)-(wallWidth), 2*(gridModifier),  2*(gridModifier)+(wallWidth), 2*(gridModifier),  2*(gridModifier)+(wallWidth), 3*(gridModifier),  2*(gridModifier)-(wallWidth), 3*(gridModifier));
+
+
+
+
+
+
+
+
+
+
+  line(0, 0, width, 0);
+  line(0, 0, 0, height);
+  line(width, 0, width, height);
+  line(0, height, width, height);
+
+//  line(1*(gridModifier), 1*(gridModifier), 2*(gridModifier), 1*(gridModifier));
+//  line(2*(gridModifier), 1*(gridModifier), 2*(gridModifier), 2*(gridModifier));
+pop();
   if (state === `title`) {
     title();
   } else if (state === `simulation`) {
@@ -80,48 +134,50 @@ function title() {
 }
 
 function simulation() {
-  circles();
-  circleCollision()
+  ellipse(player.x, player.y, player.size);
 }
 
-//function lose() {
-//  push();
-//  textSize(48);
-//  fill(150, 255, 150);
-//  textAlign(CENTER, CENTER);
-//  text(`You lose, try again`, width / 2, height / 2);
-//  pop();
-//}
+function lose() {
+  push();
+  textSize(48);
+  fill(150, 255, 150);
+  textAlign(CENTER, CENTER);
+  text(`You lose, try again`, width / 2, height / 2);
+  player.speed = 0;
+  pop();
+}
 
 function win() {
   push();
   textSize(48);
   fill(150, 255, 150);
   textAlign(CENTER, CENTER);
-  text(`You made it, congratulations`, width / 2, height / 2);
+  text(`Congratulations, You won!`, width / 2, height / 2);
   pop();
 }
 
-function circleSetup() {
-// starting location for circles
+function keyPressed() {
+  if (keyCode === 32) {
+      player.y = height;
+//    value = 255;
+//  } else {
+//    value = 0;
+  }
 }
 
 
 function borderCollisionplayer() {
-  // checks if a player collides with the border of the Canvas and changes it's speed
-  if (player.x < 0) {
-    player.x = 0;
-  }
-  if (player.x > width) {
-    player.x = width;
-  }
-  if (player.y < 0) {
-    player.y = 0;
-  }
-  if (player.y > height) {
-    player.y = height;
-  }
+  // Constraints player to the border of the Canvas
+  player.x = constrain(player.x, 10, width - 10);
+  player.y = constrain(player.y, 10, height - 10);
 
+}
+
+function wallCollisionplayer() {
+  if (player.x <= horizontalWall1.x2 && player.x >= horizontalWall1.x1 && player.y >= horizontalWall1.y1 && player.y <= horizontalWall1.y1 + player.size) {
+    lose();
+
+  }
 }
 
 
@@ -131,13 +187,10 @@ function mousePressed() {
   }
 }
 
-function circles() {
-  ellipse(player.x, player.y, player.size);
-}
 
-function circleCollision() {
-  let d = dist(player.x, player.y, entity2.x, entity2.y);
-  if (d < player.size / 2 + entity2.size / 2) {
-    state = `win`;
-  }
-}
+//function circleCollision() {
+//  let d = dist(player.x, player.y, entity2.x, entity2.y);
+//  if (d < player.size / 2 + entity2.size / 2) {
+//    state = `win`;
+//  }
+//}
